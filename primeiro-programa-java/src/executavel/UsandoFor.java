42189e39bb626bd02faf3c9cd677c55ccd33246e
Ruvian Souza <ruvian.fabrin@hotmail.com>
2022-06-08 22:53:11 -0300

    Usando o For

---
+++ primeiro-programa-java/src/executavel/UsandoFor.java
@@ -0,0 +1,12 @@
+package executavel;
+
+public class UsandoFor {
+
+	public static void main(String[] args) {
+		//for comum
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Número: "+i);
+		}
+	}
+
+}
