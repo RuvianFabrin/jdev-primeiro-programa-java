e08387604f9b951507d4ba5eb1ad423c8d92495f
Ruvian Souza <ruvian.fabrin@hotmail.com>
2022-06-22 22:15:52 -0300

    Removendo Disciplina

--- primeiro-programa-java/src/cursojava/classes/Executando.java
+++ primeiro-programa-java/src/cursojava/classes/Executando.java
@@ -13,6 +13,27 @@ public class Executando {
 		aluno.setNome(nome);
 		aluno.setIdade(Integer.valueOf(idade));
 		
+		for (int i = 0; i < 4; i++) {
+			
+			String disp = JOptionPane.showInputDialog("Qual a disciplina "+i+":");
+			String nota = JOptionPane.showInputDialog("Qual a nota:");
+			
+			Disciplina disciplinas = new Disciplina();
+			disciplinas.setDisciplina(disp);
+			disciplinas.setNota(Integer.valueOf(nota));
+			aluno.getDisciplinas().add(disciplinas);
+			//continuar = JOptionPane.showInputDialog("Deseja inserir uma Disciplina?(S/N):");
+		}
+		//Yes:0 | No:1
+		int escolha = JOptionPane.showConfirmDialog(null, "Deseja remover alguma disciplina?");
+		if(escolha==0) {
+			String disciplinaRemover = JOptionPane.showInputDialog("Qual a Disciplina?");
+			aluno.getDisciplinas().remove(Integer.valueOf(disciplinaRemover).intValue());
+		}
+		
+		
+		
+		/*
 		Disciplina disciplinas = new Disciplina();
 		
 		disciplinas.setDisciplina("Banco de dados");
@@ -29,9 +50,11 @@ public class Executando {
 		
 		disciplinas3.setDisciplina("JWT");
 		disciplinas3.setNota(98);
-		aluno.getDisciplinas().add(disciplinas3);
+		aluno.getDisciplinas().add(disciplinas3);*/
 		
-		JOptionPane.showInternalMessageDialog(null, "Nome : "+aluno.getNome()+" e a idade é "+aluno.getIdade());
+		JOptionPane.showInternalMessageDialog(null, "Nome : "+aluno.getNome()
+						+" e a idade é "+aluno.getIdade() 
+						+" e a média é "+aluno.getMedia());
 		
 	}
 }
